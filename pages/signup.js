3b683f2b07bb65992fdd372e6584fc13ddfb5c80
code-commit-at-711830799325 <code-commit-at-711830799325>
2021-01-27 16:37:34 -0800

    finish basic styling of signup page

--- pages/signup.js
+++ pages/signup.js
@@ -1,5 +1,25 @@
+import { Button, Form, Icon, Message, Segment } from 'semantic-ui-react';
+import Link from 'next/link';
+
 function Signup() {
-  return <>signup</>;
+  return <>
+    <Message attached icon="settings" header="Get Started" content="Create a new account" color="teal" />
+    <Form>
+      <Segment>
+        <Form.Input fluid icon="user" iconPosition="left" label="Name" placeholder="Name" name="name" />
+        <Form.Input fluid icon="envelope" iconPosition="left" label="Email" placeholder="Email" name="email" />
+        <Form.Input fluid icon="lock" iconPosition="left" label="Password" placeholder="Password" name="password" />
+        <Button icon="signup" type="submit" color="orange" content="Sign Up" />
+      </Segment>
+    </Form>
+    <Message attached="bottom" warning>
+      <Icon name="help" />
+      Existing user?{" "}
+      <Link href="/login">
+        <a>Log in here</a>
+      </Link>{" "}instead.
+    </Message>
+  </>;
 }
 
 export default Signup;
